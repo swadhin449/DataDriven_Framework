0b7f79a5ec225dfd9c991f3e6b72ea3dadc4b457
Swadhin Kumar Jena <jswadhin449@gmail.com>
2024-12-20 16:42:10 +0530

    init commit

--- src/test/java/org/automation/base/WaitStatement.java
+++ src/test/java/org/automation/base/WaitStatement.java
@@ -1,13 +1,6 @@
 package org.automation.base;
 
-import org.automation.utility.Utility;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.io.IOException;
-import java.time.Duration;
 
 public class WaitStatement {
     WebDriver driver;
