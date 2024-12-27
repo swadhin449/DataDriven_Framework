10e913b60ad4f4316c523473e61d25459c88f774
Swadhin Kumar Jena <jswadhin449@gmail.com>
2024-12-27 17:44:16 +0530

    New changes commit

--- src/test/java/org/automation/datagenerators/DataGenerator2.java
+++ src/test/java/org/automation/datagenerators/DataGenerator2.java
@@ -2,27 +2,28 @@ package org.automation.datagenerators;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.automation.utility.Utility;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class DataGenerator2 {
     public static void main(String[] args) throws Exception {
-        //generateMainData();
         try {
             DataGenerator2 d2 = new DataGenerator2(Utility.fetchExcelValue("Excel3"));
+            //DataGenerator2 d2 = new DataGenerator2(Utility.fetchExcelValue("Excel4"));
             System.out.println("Number Of Rows: "+d2.getRowValue("Sheet1"));
             System.out.println("Number of Cells on Each Row: "+d2.getCellValues("Sheet1"));
             d2.generateCellData("Sheet1");
         } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("Exception - File is not available at the specified location."+ "\n" +e.getMessage());
         }
     }
+
+
     public FileInputStream fis;
     public XSSFWorkbook wb;
     public XSSFSheet sheet;
@@ -39,7 +40,7 @@ public class DataGenerator2 {
         // Check if the sheet is null or has no rows
         if (sheet == null || sheet.getPhysicalNumberOfRows() == 0) {
             System.out.println("The sheet is empty.");
-            return -1; // Returning -1 to indicate the sheet is empty
+            return 0; // Returning 0 to indicate the sheet is empty
         }
         //int numOfRows = sheet.getLastRowNum();//Index of Rows
         int numOfRows = sheet.getPhysicalNumberOfRows();//Length of Rows
@@ -66,45 +67,36 @@ public class DataGenerator2 {
     }
 
     public void generateCellData(String sheetName) throws Exception {
-        fis = new FileInputStream(path);
-        wb = new XSSFWorkbook(fis);
-        sheet = wb.getSheet(sheetName);
-
-        int numOfRows = sheet.getPhysicalNumberOfRows();//Length of Rows
-        Row row = sheet.getRow(numOfRows - 1);
-        int numOfCell = 0;
-        if (row != null) {
-            numOfCell = row.getPhysicalNumberOfCells();
-        }
-
-        for (int i = 0; i < numOfRows; i++) {
-            String p1 = null;
-            for (int j = 0; j < numOfCell; j++) {
-                Cell cell = sheet.getRow(i).getCell(j, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK); // Get cell and handle null
-                if (cell != null) {
-                    // Get the type of the cell and switch accordingly
-                    switch (cell.getCellType()) {
-                        case STRING:
-                            p1 = cell.getStringCellValue();
-                            break;
-                        case NUMERIC:
-                            p1 = String.valueOf(cell.getNumericCellValue());
-                            break;
-                        case BOOLEAN:
-                            p1 = String.valueOf(cell.getBooleanCellValue());
-                            break;
-                        case BLANK:
-                            p1 = "No Data";
-                            break;
-                        default:
-                            p1 = "Unknown Type"; // For other unsupported cell types
-                            break;
-                    }
-                }
-                System.out.print(p1 + " ");  // Print the value of the cell (p1)
+            fis = new FileInputStream(path);
+            wb = new XSSFWorkbook(fis);
+            sheet = wb.getSheet(sheetName);
+            int numOfRows = sheet.getPhysicalNumberOfRows();//Length of Rows
+            if (sheet == null || sheet.getPhysicalNumberOfRows() == 0) {
+                System.out.println("The sheet is empty.");
             }
-            System.out.println(); // New line after each row
-        }
+            assert sheet != null;// Check if the sheet is null
+            Row row = sheet.getRow(numOfRows - 1);
+            int numOfCell = 0;
+            if (row != null) {
+                numOfCell = row.getPhysicalNumberOfCells();
+            }
+            for (int i = 0; i < numOfRows; i++) {
+                String p1 = null;
+                for (int j = 0; j < numOfCell; j++) {
+                    Cell cell = sheet.getRow(i).getCell(j, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK); // Get cell and handle null
+                    if (cell != null) {
+                        // Get the type of the cell and switch accordingly
+                        p1 = switch (cell.getCellType()) {
+                            case STRING -> cell.getStringCellValue();
+                            case NUMERIC -> String.valueOf(cell.getNumericCellValue());
+                            case BOOLEAN -> String.valueOf(cell.getBooleanCellValue());
+                            case BLANK -> "No Data";
+                            default -> "Unknown Type"; // For other unsupported cell types
+                        };
+                    }
+                    System.out.print(p1 + " ");  // Print the value of the cell (p1)
+                }
+                System.out.println(); // New line after each row
+            }
     }
-
 }
